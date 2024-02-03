7a4352d19a7cb46e3a2f88251e3cc0a45754e5aa
Welder Fernandes <welderx3@gmail.com>
2024-02-03 16:42:44 -0300

    feat: add initial header component

---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+function Header() {
+  return (
+    <Card>
+      <CardContent className="flex justify-between p-5 items-center">
+        <Image src="/Logo.png" alt="SIMP Logo" width={120} height={22} />
+        <Button variant="outline" size="icon" className="h-8 w-8">
+          <MenuIcon size={16} />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Header
