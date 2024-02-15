0e3dadf7f2974eae76e358f0d6a863a30bb3e47c
Welder Fernandes <welderx3@gmail.com>
2024-02-15 16:58:31 -0300

    fix: emcorpored alternative project

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
         hostname: "utfs.io",
-        port: "",
-        pathname: "/**",
       },
     ],
-  }
+  },
 };
 
 export default nextConfig;
--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="bg-secondary py-6 px-5">
-      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <p className="text-sm text-gray-400 opacity-75">Copyright © 2022 - All rights reserved</p>
-      </div>
-    </footer>
-  )
-}
